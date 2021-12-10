abe946366dc6f4bd8acff6f67bff227dc62b5f79
samuelmmjr <samuelmmjunior@gmail.com>
2021-12-10 14:33:16 -0300

    retornando resultado em uma variavel

--- controllers/numbers.js
+++ controllers/numbers.js
@@ -1,23 +1,23 @@
 const axios = require('axios');
-const numbersServices = require('../services/numbers');
 const { mergeSort } = require('../utils/orderNumbers')
 
 const extract = async(req, res) => {
     let index = 1
+    let numbers = []
 
     try {
         while(true) {
             const { data } = await axios(`http://challenge.dienekes.com.br/api/numbers?page=${index}`)
-            await numbersServices.create(data.numbers)
+            numbers.push(...data.numbers)
             index += 1
             if (data.numbers.length === 0) break
         }
+
     } catch(error) {
         console.error(error.response)
     } finally {
-        const numbersDb = await numbersServices.getAll()
-        mergeSort(numbersDb, 0, numbersDb.length - 1)
-        res.status(200).json(numbersDb)
+        mergeSort(numbers, 0, numbers.length - 1)
+        res.status(200).json(numbers)
     }
 }   
 
