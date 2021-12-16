2a2d10c91093decfcaa092b8f467815fc38686b2
samuelmmjr <samuelmmjunior@gmail.com>
2021-12-15 22:59:41 -0300

    Fix: tolerancia a falha

--- controllers/numbers.js
+++ controllers/numbers.js
@@ -1,24 +1,24 @@
 const axios = require('axios');
 const { mergeSort } = require('../utils/orderNumbers')
 
-const extract = async(req, res) => {
+const extract = async (req, res) => {
     let index = 1
     let numbers = []
 
-    try {
-        while(true) {
+
+    while (true) {
+        try {
             const { data } = await axios(`http://challenge.dienekes.com.br/api/numbers?page=${index}`)
             numbers.push(...data.numbers)
+            console.log(index)            
             index += 1
             if (data.numbers.length === 0) break
+        } catch (error) {
+            console.error(error.response)
         }
-
-    } catch(error) {
-        console.error(error.response)
-    } finally {
-        mergeSort(numbers, 0, numbers.length - 1)
-        res.status(200).json(numbers)
     }
-}   
+    mergeSort(numbers, 0, numbers.length - 1)
+    res.status(200).json(numbers)
+}
 
 module.exports = { extract };
